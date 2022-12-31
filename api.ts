0973d20b01c8ff6c7ae9192c4f076bcdd79e4cc3
RoBlockHead <15642007+RoBlockHead@users.noreply.github.com>
2022-12-30 23:33:24 -0500

    Add basic arrivals and positions functionality

--- api.ts
+++ api.ts
@@ -3,4 +3,17 @@ export class CtaClient {
     constructor(apiKey: string) {
         this.apiKey = apiKey;
     }
+    public async getArrivals(stationId: number): Promise<unknown> {
+        const url = `http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${this.apiKey}&mapid=${stationId}&outputType=JSON`;
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    }
+    public async getPositions(routes: string[]): Promise<unknown> {
+        const url = `http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=${this.apiKey}&rt=${routes.join(",")}&outputType=JSON`;
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    }
+
 }
